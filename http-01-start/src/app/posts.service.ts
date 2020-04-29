9e8e3fdab486b9f3c1b57d85a0e99ce6806c909b
jay <jay.subramanian@zcomsystems.com>
2020-04-29 11:55:46 -0400

    http

---
+++ http-01-start/src/app/posts.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders, HttpParams,HttpEventType } from '@angular/common/http';
+import { Post } from './post.model';
+import { map, catchError,tap } from 'rxjs/operators';
+import { Subject,throwError } from 'rxjs';
+
+@Injectable({providedIn:"root"})
+export class PostsService{
+
+    error= new Subject<string>();
+    constructor(private http :HttpClient){
+
+    }
+
+    createAndStorePost(title: string,content: string){
+       
+        const postData: Post = {title:title,content:content}
+     this.http
+      .post<{name: string}>(
+        'https://angular-db-6b00a.firebaseio.com/posts.json',
+        postData,{
+            observe:'response'
+        }
+        
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      },error =>{
+          this.error.next(error.message);
+      }); 
+    }
+
+    fetchPosts(){
+        let searchParams = new HttpParams();
+        searchParams = searchParams.append('print', 'pretty');
+        searchParams = searchParams.append('custom', 'key');
+       return this.http.get<{[key: string] : Post} >('https://angular-db-6b00a.firebaseio.com/posts.json',
+       {
+            headers: new HttpHeaders({'Custom-header':'Hello'}),
+            params: searchParams
+       })
+        .pipe(map( (responseData) =>{
+          const postsArray: Post[] = [];
+          for(const key in responseData){
+            if(responseData.hasOwnProperty(key)){
+              postsArray.push({...responseData[key],id: key}) 
+            }
+           
+          }
+          return postsArray;
+        }),
+    catchError((errorRes)=>{
+        //send to analytics server
+        return throwError(errorRes);
+    })) 
+        
+    }
+
+    deletePosts(){
+       return this.http.delete('https://angular-db-6b00a.firebaseio.com/posts.json',
+    {
+        observe: 'events',
+        responseType: 'json'
+    }).pipe(tap(event => {
+        console.log(event);
+        if(event.type === HttpEventType.Sent){
+            // 
+        }
+        if(event.type === HttpEventType.Response){
+               console.log(event.body); 
+        }
+    }));
+
+        
+    }
+
+
+}
